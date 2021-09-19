bf52ae8461d3593d29742d0091858d453add8842
pchenr1que1 <pchenr1que1@gmail.com>
2021-09-19 14:41:35 -0300

    checkpoint

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,18 @@
-export function Content() {
+interface GenreResponseProps {
+  selectedGenre: {
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  };
+}
+
+export function Content(props: GenreResponseProps) {
   // Complete aqui
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {props.selectedGenre.title}</span>
+      </span>
+    </header>
+  );
 }
